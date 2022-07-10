13d3b495b8cc18e5fe78fbe6ef4a0a1f466d23e9
Ivo Donchev <i.donchev@hacksoft.io>
2022-07-10 10:54:27 +0300

    Format util

--- python/multiprocessing/utils.py
+++ python/multiprocessing/utils.py
@@ -1,7 +1,10 @@
-def func(process_idx):
+def func(id):
     total_iterations = 100000000
     step = total_iterations / 10
 
     for i in range(1, total_iterations):
         if i % step == 0:
-            print(f'Process {process_idx} reached {(i / step) * 10} %')
+            progress = (i / step) * 10
+            print(
+                f'Process {id} reached {progress} %'
+            )
